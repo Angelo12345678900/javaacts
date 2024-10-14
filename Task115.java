572613916d331c67b1f88525fefecb940f30b527
Angelo12345678900 <156929722+Angelo12345678900@users.noreply.github.com>
2024-10-14 22:46:57 +0800

    Create Task115.java

---
+++ Task115.java
@@ -0,0 +1,12 @@
+String a = "Wow!";
+String b = "Wow";
+String c = new String("Different");
+String d = a;
+
+boolean b1 = a == b;
+boolean b2 = d.equals(b + "!");
+boolean b3 = !c.equals(a);
+
+if (b1 && b2 && b3) {
+    System.out.println("Success!");
+}
