d11613d2adde82d38e7fe8a938cb8a2c483de12c
Angelo12345678900 <156929722+Angelo12345678900@users.noreply.github.com>
2024-10-14 23:07:10 +0800

    Update Task115.java

--- Task115.java
+++ Task115.java
@@ -1,12 +1,16 @@
-String a = "Wow!";
-String b = "Wow";
-String c = new String("Different");
-String d = a;
+public class Task115 {
+    public static void main(String[] args) {
+        String a = "Wow";
+        String b = a;
+        String c = b + "!";
+        String d = c;
 
-boolean b1 = a == b;
-boolean b2 = d.equals(b + "!");
-boolean b3 = !c.equals(a);
+        boolean b1 = a == b;
+        boolean b2 = d.equals(b + "!");
+        boolean b3 = !c.equals(a);
 
-if (b1 && b2 && b3) {
-    System.out.println("Success!");
+        if (b1 && b2 && b3) {
+            System.out.println("Success!");
+        }
+    }
 }
